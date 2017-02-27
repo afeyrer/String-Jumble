d7864a9f828f373726f0d28c3de893d96e753eba
afeyrer <abigailfeyrer@hanovernorwichschools.org>
2017-02-27 14:54:46 -0500

    Updated from Brython Server: 2/27/2017 2:54:45 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,5 +25,11 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+
+import string
 txt=input("Please enter a string of text (the bigger the better): ")
 print('you entered "'+txt+'". Now jumble it: ')
+
+
+
+print(string.ascii_lowercase)
